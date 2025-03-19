e03711aee540cac5511abaf76e18496818a82de8
Joel Williams <joel@reboundstudios.com>
2025-03-19 09:29:51 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen w-screen justify-center items-center'>
+      <div className='w-3xl h-full mx-auto'>
+        <h1>Hello, my name is Joel!</h1>
+        <p>Here is what I'm working on</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
