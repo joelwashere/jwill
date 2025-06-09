41d3905bd98066cac0d7b4d02c1bd5d1a77a653f
Joel Williams <joel@reboundstudios.com>
2025-06-09 14:24:44 +0900

    updated bio

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './App.css'
 
 interface ProjectInfo {
   name: string,
-  image: string,
-  description?: string
+  image?: string,
+  description?: string,
+  link?: string
 }
 
-function Project({ name, image, description }: ProjectInfo) {
+function Project({ name, image, description, link }: ProjectInfo) {
   return (
-    <a href='#' className='w-full'>
+    <a href={link} className='w-full'>
       <div className='w-full rounded border px-4 py-4 flex justify-between'>
         <div className=''>
           <p className='text-xl mb-2'>{name}</p>
@@ -34,8 +35,8 @@ function App() {
       <div className='font-lora h-screen w-screen flex flex-col *:w-3xl *:mx-auto'>
         <div className='h-fit pt-10 border-b'>
           <h1 className='mb-2'>Joel Williams</h1>
-          <p className='mt-2 mb-3'>📍 Washington, DC</p>
-          <p className='text-lg'>Hi my name is Joel, I am currently studying Economics and I enjoy building games and simulators as a hobby. I spent the last two years in sales and I am now pursuing building the future of technology.</p>
+          <p className='mt-2 mb-3'>📍 Tokyo, Japan</p>
+          <p className='text-lg'>Hi my name is Joel, I am currently studying Economics and I enjoy building games and simulators as a hobby. I spent the last two years in sales and now I am building products that help move the world forward.</p>
           <ul className='font-sans flex gap-4 my-4'>
             <li><a href='https://github.com/joelwashere' target='_blank' className='border py-1 px-2 rounded'>👨‍💻 GitHub</a></li>
             {/* <li><a href='#' target='_blank' className='border py-1 px-2 rounded'>💼 LinkedIn</a></li>
@@ -45,8 +46,9 @@ function App() {
         </div>
         <div className='mt-3'>
           <h2 className='text-xl'>Projects</h2>
-          <div className='gap-4 mt-2'>
-            <Project name={'Graph'} image={'src/assets/react.svg'} description='A graphing calculator' />
+          <div className='flex flex-col gap-4 mt-2'>
+            <Project name={'Graph'} image={'src/assets/react.svg'} description='A graphing calculator' link="https://interpretbetter.com/graph" />
+            <Project name={'Nemo'} description='A people search tool' link="#" />
           </div>
         </div>
       </div>
